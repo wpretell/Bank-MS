c4d13a0c5adc58c0d391dea3daf65808f9c8de20
Walter Pretell <wpretell@outlook.com>
2022-09-14 12:08:44 -0500

    Se corrige mODEL

--- BankClient/src/main/java/com/Bootcamp/BankClient/web/model/ClientProfileModel.java
+++ BankClient/src/main/java/com/Bootcamp/BankClient/web/model/ClientProfileModel.java
@@ -1,7 +1,16 @@
 package com.Bootcamp.BankClient.web.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import javax.persistence.Id;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class ClientProfileModel {
 
     private String clientProfileId;
